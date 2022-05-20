b2e17cf431872c7687b64bd667e70ed4271f2ada
Herman Wikner <wiknerherman@gmail.com>
2022-05-20 18:04:27 +0200

    curved paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,47 @@
-import {Card, Container, Grid} from '@sanity/ui'
+import {Card, Grid} from '@sanity/ui'
 import {motion} from 'framer-motion'
-import {useCallback} from 'react'
-import {Connect, useConnectElements} from './connect-lines'
+import {Connect} from './connect-lines'
 
 const MotionCard = motion(Card)
 
 export default function App() {
-  const {addElement} = useConnectElements()
-
-  const addEl = useCallback(
-    (node) => {
-      addElement({
-        element: node,
-        id: 'with-hook',
-        connectWith: ['motion-card'],
-      })
-    },
-    [addElement]
-  )
-
   return (
-    <Card height="fill">
-      <Container padding={6} sizing="border">
-        <Grid columns={2} gap={8}>
-          <Connect id="card-1" connectWith={['card-2', 'motion-card']} color="lightseagreen">
-            <Card padding={6} border tone="primary">
-              Card 1
-            </Card>
-          </Connect>
+    <Grid padding={8} columns={2} gap={7}>
+      <Connect id="card-1">
+        <Card tone="primary" border padding={3}>
+          Card 1
+        </Card>
+      </Connect>
 
-          <Connect id="card-2" connectWith={['card-4']}>
-            <Card padding={6} border tone="positive">
-              Card 2
-            </Card>
-          </Connect>
+      <Connect id="card-2" color="lightseagreen" connectWith={['card-3']}>
+        <Card tone="primary" border padding={3}>
+          Card 2
+        </Card>
+      </Connect>
 
-          <Connect id="motion-card">
-            <MotionCard padding={6} border tone="caution" drag dragMomentum={false}>
-              Motion Card 3
-            </MotionCard>
-          </Connect>
+      <Connect id="card-3" connectWith={['card-5']} color="blue">
+        <Card tone="primary" border padding={3}>
+          Card 3
+        </Card>
+      </Connect>
 
-          <Connect id="card-4" connectWith={['card-3', 'motion-card']}>
-            <Card padding={6} border tone="critical">
-              Card 4
-            </Card>
-          </Connect>
+      <Connect id="card-4" connectWith={['card-3']}>
+        <MotionCard tone="primary" border padding={4} drag dragMomentum={false}>
+          Card 4
+        </MotionCard>
+      </Connect>
 
-          <Card padding={6} border tone="critical" ref={addEl}>
-            With hook
-          </Card>
-        </Grid>
-      </Container>
-    </Card>
+      <Connect id="card-5" color="red">
+        <Card tone="primary" border padding={3}>
+          Card 5
+        </Card>
+      </Connect>
+
+      <Connect id="card-6" connectWith={['card-3']} color="blue">
+        <Card tone="primary" border padding={3}>
+          Card 6
+        </Card>
+      </Connect>
+    </Grid>
   )
 }
--- src/connect-lines/lines/getPathData.ts
+++ src/connect-lines/lines/getPathData.ts
@@ -5,67 +5,128 @@ interface GetPathDataProps {
 
 const OFFSET = 7
 
+const pathify = (paths: {x: string | number; y: string | number}[]) =>
+  `M ${paths.map((p) => `${p.x} ${p.y}`).join(' ')}`
+
 export function getPathData(props: GetPathDataProps) {
   const {from, to} = props
 
   if (!from || !to) return
 
-  let tmp
-
+  // right => left
   if (from.left > to.right) {
-    tmp = [
+    const rightToLeft = [
       {
         x: from?.left,
-        y: from?.top + from.height / 2,
+        y: from?.bottom - from.height / 2,
+      },
+      {
+        x: `C ${(to.right + from.left) / 2}`,
+        y: from?.bottom - from.height / 2,
+      },
+      {
+        x: (to.right + from.left) / 2,
+        y: to.top + to.height / 2,
       },
       {
         x: to.right + OFFSET,
         y: to.top + to.height / 2,
       },
+      // Duplicate to make the arrow point at the right direction
+      {
+        x: to.right + OFFSET,
+        y: to.top + to.height / 2,
+      },
     ]
+
+    return pathify(rightToLeft)
   }
 
+  // left => right
   if (from.right < to.left) {
-    tmp = [
+    const leftToRight = [
       {
         x: from?.right,
-        y: from?.top + from.height / 2,
+        y: from?.bottom - from.height / 2,
+      },
+      {
+        x: `C ${(to.left + from.right) / 2}`,
+        y: from?.bottom - from.height / 2,
+      },
+      {
+        x: (to.left + from.right) / 2,
+        y: to.top + to.height / 2,
       },
       {
         x: to.left - OFFSET,
         y: to.top + to.height / 2,
       },
+      // Duplicate to make the arrow point at the right direction
+      {
+        x: to.left - OFFSET,
+        y: to.top + to.height / 2,
+      },
     ]
+
+    return pathify(leftToRight)
   }
 
+  // bottom => top
   if (from.bottom < to.top) {
-    tmp = [
+    const bottomToTop = [
       {
         x: from?.left + from.width / 2,
         y: from?.bottom,
       },
       {
-        x: to.left + to.width / 2,
+        x: `C ${from?.left + from.width / 2}`,
+        y: from.bottom - (from.bottom - to.top) / 2,
+      },
+      {
+        x: to?.left + to.width / 2,
+        y: from.bottom - (from.bottom - to.top) / 2,
+      },
+      {
+        x: to?.left + to.width / 2,
+        y: to.top - OFFSET,
+      },
+      // Duplicate to make the arrow point at the right direction
+      {
+        x: to?.left + to.width / 2,
         y: to.top - OFFSET,
       },
     ]
+
+    return pathify(bottomToTop)
   }
 
+  // top => bottom
   if (from.top > to.bottom) {
-    tmp = [
+    const topToBottom = [
       {
         x: from?.left + from.width / 2,
         y: from?.top,
       },
       {
-        x: to.left + to.width / 2,
+        x: `C ${from?.left + from.width / 2}`,
+        y: from.top - (from.top - to.bottom) / 2,
+      },
+      {
+        x: to?.left + to.width / 2,
+        y: from.top - (from.top - to.bottom) / 2,
+      },
+      {
+        x: to?.left + to.width / 2,
+        y: to.bottom + OFFSET,
+      },
+      // Duplicate to make the arrow point at the right direction
+      {
+        x: to?.left + to.width / 2,
         y: to.bottom + OFFSET,
       },
     ]
-  }
 
-  if (tmp) {
-    return `M ${tmp.map((p) => `${p.x} ${p.y}`).join(' ')}`
+    return pathify(topToBottom)
   }
 
   return undefined
