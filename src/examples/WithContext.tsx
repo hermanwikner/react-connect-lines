133ce135735c64e1ebfc6659b37a639803a13c31
Herman Wikner <wiknerherman@gmail.com>
2022-07-04 15:51:30 +0200

    exAMPLE

--- src/examples/WithContext.tsx
+++ src/examples/WithContext.tsx
@@ -1,13 +1,11 @@
-import {AddIcon, EditIcon} from '@sanity/icons'
+import {AddIcon, EditIcon, TrashIcon} from '@sanity/icons'
 import {
   Box,
   Button,
   Card,
-  Checkbox,
   Container,
   Flex,
   Grid,
-  Inline,
   Label,
   Menu,
   MenuButton,
@@ -17,28 +15,18 @@ import {
   Switch,
   Text,
 } from '@sanity/ui'
-import {AnimatePresence, motion} from 'framer-motion'
-import React, {useCallback, useState} from 'react'
+import {motion} from 'framer-motion'
+import {useCallback, useState} from 'react'
 import {Connect, ConnectProvider, ConnectElement} from '../../pkg'
+import {randomEmojiId} from './randomId'
 
 const MotionCard = motion(Card)
 
-const randId = () => Math.random().toString(36).slice(2)
-
-const INITIAL: ConnectElement[] = [
-  {
-    id: randId(),
-  },
-  {
-    id: randId(),
-  },
-]
-
 export function WithContext() {
-  const [connections, setConnections] = useState<ConnectElement[]>(INITIAL)
+  const [connections, setConnections] = useState<ConnectElement[]>([])
 
   const add = useCallback(() => {
-    setConnections((prev) => [...prev, {id: randId()}])
+    setConnections((prev) => [...prev, {id: randomEmojiId()}])
   }, [])
 
   const handleConnect = useCallback(
@@ -96,6 +84,15 @@ export function WithContext() {
     [connections]
   )
 
+  const handleDelete = useCallback(
+    (id: string) => {
+      const update = connections.filter((c) => c.id !== id)
+
+      setConnections(update)
+    },
+    [connections]
+  )
+
   return (
     <ConnectProvider>
       <Container width={3} paddingX={7} paddingY={5} sizing="border">
@@ -104,7 +101,7 @@ export function WithContext() {
         </Flex>
 
         <Grid columns={2} gapY={7} gapX={9}>
-          {connections.map((c) => (
+          {connections.map((c, index) => (
             <Connect id={c.id} connectWith={c?.connectWith} key={c.id}>
               <MotionCard
                 padding={4}
@@ -112,129 +109,151 @@ export function WithContext() {
                 radius={2}
                 drag
                 dragMomentum={false}
-                initial={{scale: 0.5}}
-                animate={{scale: 1}}
+                initial={{scale: 0.5, y: 0}}
+                animate={{scale: 1, y: index * 50}}
+                style={{position: 'absolute'}}
               >
                 <Stack space={4}>
-                  <Flex align="center" gap={2}>
+                  <Flex align="center" justify="space-between" gap={2}>
                     <Box>
                       <Text align="center" muted weight="bold">
                         {c.id}
                       </Text>
                     </Box>
 
-                    <MenuButton
-                      id="menu"
-                      button={<Button icon={EditIcon} mode="ghost" fontSize={1} padding={2} />}
-                      popover={{portal: true, constrainSize: true}}
-                      menu={
-                        <Menu padding={0}>
-                          <Box padding={3}>
-                            <Stack space={4}>
-                              {/* Connect with */}
-                              <Stack space={3}>
-                                {connections
-                                  .filter((i) => i.id !== c?.id)
-                                  ?.map((x, index) => {
-                                    return (
-                                      <React.Fragment key={x.id}>
-                                        <Flex gap={3}>
-                                          <Stack space={2}>
+                    <Flex gap={2} marginLeft={7}>
+                      <Button
+                        icon={TrashIcon}
+                        mode="ghost"
+                        fontSize={1}
+                        padding={2}
+                        onClick={() => handleDelete(c.id)}
+                      />
+
+                      <MenuButton
+                        id="menu"
+                        button={
+                          <Button
+                            icon={EditIcon}
+                            mode="ghost"
+                            fontSize={1}
+                            padding={2}
+                            disabled={connections.length === 1}
+                          />
+                        }
+                        popover={{portal: true, constrainSize: true}}
+                        menu={
+                          <Menu padding={0}>
+                            <Box padding={3}>
+                              <Stack space={4}>
+                                {/* Connect with */}
+                                <Stack space={3}>
+                                  {connections
+                                    .filter((i) => i.id !== c?.id)
+                                    ?.map((x, index) => {
+                                      return (
+                                        <Stack key={x.id} space={4}>
+                                          <Text weight="bold" size={1}>
+                                            {x.id}
+                                          </Text>
+                                          <Flex gap={3}>
+                                            <Stack space={2}>
+                                              <Stack space={2}>
+                                                <Label size={0} muted>
+                                                  Connected
+                                                </Label>
+
+                                                <Box style={{height: 25}}>
+                                                  <Switch
+                                                    onChange={(e) => handleConnect(e, c, x)}
+                                                    checked={Boolean(
+                                                      c.connectWith?.find((y) => y.id === x.id)
+                                                    )}
+                                                  />
+                                                </Box>
+                                              </Stack>
+                                            </Stack>
+
                                             <Stack space={2}>
                                               <Label size={0} muted>
-                                                Connected
+                                                Stroke
                                               </Label>
 
-                                              <Box style={{height: 25}}>
-                                                <Switch
-                                                  onChange={(e) => handleConnect(e, c, x)}
-                                                  checked={Boolean(
-                                                    c.connectWith?.find((y) => y.id === x.id)
-                                                  )}
-                                                />
-                                              </Box>
+                                              <Select
+                                                fontSize={1}
+                                                padding={2}
+                                                defaultValue={
+                                                  c.connectWith?.find((y) => y.id === x.id)
+                                                    ?.stroke || 'solid'
+                                                }
+                                                onChange={(e) =>
+                                                  handleUpdate(c, x, {
+                                                    stroke: e.currentTarget.value,
+                                                  })
+                                                }
+                                              >
+                                                <option value="dashed">Dashed</option>
+                                                <option value="solid">Solid</option>
+                                              </Select>
                                             </Stack>
-                                          </Stack>
 
-                                          <Stack space={2}>
-                                            <Label size={0} muted>
-                                              Stroke
-                                            </Label>
+                                            <Stack space={2}>
+                                              <Label size={0} muted>
+                                                Edge
+                                              </Label>
 
-                                            <Select
-                                              fontSize={1}
-                                              padding={2}
-                                              defaultValue={
-                                                c.connectWith?.find((y) => y.id === x.id)?.stroke ||
-                                                'solid'
-                                              }
-                                              onChange={(e) =>
-                                                handleUpdate(c, x, {
-                                                  stroke: e.currentTarget.value,
-                                                })
-                                              }
-                                            >
-                                              <option value="dashed">Dashed</option>
-                                              <option value="solid">Solid</option>
-                                            </Select>
-                                          </Stack>
+                                              <Select
+                                                fontSize={1}
+                                                padding={2}
+                                                defaultValue={
+                                                  c.connectWith?.find((y) => y.id === x.id)?.edge ||
+                                                  'bezier'
+                                                }
+                                                onChange={(e) =>
+                                                  handleUpdate(c, x, {
+                                                    edge: e.currentTarget.value,
+                                                  })
+                                                }
+                                              >
+                                                <option value="bezier">Bezier</option>
+                                                <option value="step">Step</option>
+                                              </Select>
+                                            </Stack>
 
-                                          <Stack space={2}>
-                                            <Label size={0} muted>
-                                              Edge
-                                            </Label>
+                                            <Stack space={2}>
+                                              <Label size={0} muted>
+                                                Color
+                                              </Label>
 
-                                            <Select
-                                              fontSize={1}
-                                              padding={2}
-                                              defaultValue={
-                                                c.connectWith?.find((y) => y.id === x.id)?.edge ||
-                                                'bezier'
-                                              }
-                                              onChange={(e) =>
-                                                handleUpdate(c, x, {
-                                                  edge: e.currentTarget.value,
-                                                })
-                                              }
-                                            >
-                                              <option value="bezier">Bezier</option>
-                                              <option value="step">Step</option>
-                                            </Select>
-                                          </Stack>
+                                              <Card
+                                                as="input"
+                                                radius={2}
+                                                style={{height: 25}}
+                                                defaultValue={
+                                                  c.connectWith?.find((y) => y.id === x.id)
+                                                    ?.color || 'magenta'
+                                                }
+                                                onChange={(e: any) =>
+                                                  handleUpdate(c, x, {
+                                                    color: e.currentTarget.value,
+                                                  })
+                                                }
+                                                type="color"
+                                              />
+                                            </Stack>
+                                          </Flex>
 
-                                          <Stack space={2}>
-                                            <Label size={0} muted>
-                                              Color
-                                            </Label>
-
-                                            <Card
-                                              as="input"
-                                              radius={2}
-                                              style={{height: 25}}
-                                              defaultValue={
-                                                c.connectWith?.find((y) => y.id === x.id)?.color ||
-                                                'magenta'
-                                              }
-                                              onChange={(e: any) =>
-                                                handleUpdate(c, x, {
-                                                  color: e.currentTarget.value,
-                                                })
-                                              }
-                                              type="color"
-                                            />
-                                          </Stack>
-                                        </Flex>
-
-                                        {index + 2 < connections.length && <MenuDivider />}
-                                      </React.Fragment>
-                                    )
-                                  })}
+                                          {index + 2 < connections.length && <MenuDivider />}
+                                        </Stack>
+                                      )
+                                    })}
+                                </Stack>
                               </Stack>
-                            </Stack>
-                          </Box>
-                        </Menu>
-                      }
-                    />
+                            </Box>
+                          </Menu>
+                        }
+                      />
+                    </Flex>
                   </Flex>
                 </Stack>
               </MotionCard>
